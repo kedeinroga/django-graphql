8249df64a3b5dbb140a77b5687be1d0553534ef1
kedeinroga <keybroga@gmail.com>
2021-02-23 19:54:01 -0500

    created grephene_schema where later we will implement types and schema

--- djangographql/settings.py
+++ djangographql/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'graphene_django',
+    'movies',
+    'graphene_scheme',
 ]
 
 MIDDLEWARE = [
